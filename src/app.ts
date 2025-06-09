fdd6cab35dccae263575e0b19b3a9afd900817c3
Shokhzod2001 <shahzodabdumalikov1@gmail.com>
2025-06-09 22:57:18 +0900

    feat: express integration

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,32 @@
+import cors from "cors";
 import express from "express";
+import path from "path";
+// import router from "./router";
+// import routerAdmin from "./router-admin";
+// import morgan from "morgan";
+// import cookieParser from "cookie-parser";
+// import { MORGAN_FORMAT } from "./libs/config";
 
+// import session from "express-session";
+// import ConnectMongoDB from "connect-mongodb-session";
+// import { T } from "./libs/types/common";
+
+/** 1 - ENTRANCE **/
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+// app.use("/uploads", express.static("./uploads"));
+app.use(express.json());
+// app.use(cors({ credentials: true, origin: true }));
+// app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+// app.use(morgan(MORGAN_FORMAT));
+
+/** 2 - SESSIONS **/
+
+/** 3 - VIEWS **/
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+/** 4 - ROUTERS **/
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
     console.log(`Mongodb connection succeed`);
-    const PORT = process.env.PORT ?? 3003;
+    const PORT = process.env.PORT ?? 3007;
     app.listen(PORT, function () {
       console.info(`The server running succesfully on port: ${PORT}`);
       console.info(`Admin project on http://localhost:${PORT}/admin \n`);
