beca0cf081d77bd5a85cd6d1802cd59d9b902385
Shokhzod2001 <shahzodabdumalikov1@gmail.com>
2025-06-09 20:59:29 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import app from "./app";
 
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log(`Mongodb connection succeed`);
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.info(`The server running succesfully on port: ${PORT}`);
+      console.info(`Admin project on http://localhost:${PORT}/admin \n`);
+    });
+  })
+  .catch((err) => {
+    console.log(`ERROR on connection Mongodb ${err}`);
+  });
