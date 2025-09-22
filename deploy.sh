543144bcd8c2100c7522e82ad145903075f7cf0a
Shokhzod2001 <shahzodabdumalikov1@gmail.com>
2025-09-23 08:43:37 +0900

    feat: deploy project via shel and pm2

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Production
+git reset --hard
+git checkout master
+git pull origin master
+
+npm install
+npm run build
+pm2 start process.config.js --env production
+
+# Development
+# git reset --hard
+# git checkout develop
+# git pull origin develop
+
+# npm install
+# pm2 start "npm run start:dev" --name=BARAB
